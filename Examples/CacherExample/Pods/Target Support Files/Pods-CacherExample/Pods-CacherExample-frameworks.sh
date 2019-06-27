2707d43a738b01c3276f3b0cc09aded0c6d9fba7
Aashish Tamsya <aashish.tamsya@gmail.com>
2019-06-27 23:25:22 +0530

    Included Cacher using cocoapods in CacherExample project

--- Examples/CacherExample/Pods/Target Support Files/Pods-CacherExample/Pods-CacherExample-frameworks.sh
+++ Examples/CacherExample/Pods/Target Support Files/Pods-CacherExample/Pods-CacherExample-frameworks.sh
@@ -162,11 +162,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Cacher/Cacher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReachabilitySwift/Reachability.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Cacher/Cacher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReachabilitySwift/Reachability.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
 fi
