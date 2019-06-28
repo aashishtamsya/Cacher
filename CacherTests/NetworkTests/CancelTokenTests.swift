ffd50544aad3a63222f1cac3a586f88dc06b93ce
Aashish Tamsya <aashish.tamsya@gmail.com>
2019-06-28 15:20:28 +0530

    Remove unused unit test cases

--- CacherTests/NetworkTests/CancelTokenTests.swift
+++ CacherTests/NetworkTests/CancelTokenTests.swift
@@ -11,17 +11,6 @@ import XCTest
 
 final class CancelTokenTests: XCTestCase {
   
-  func test_requestToken_hashable() {
-    guard let imageURL = URL(string: "http://s75.mindvalley.us/mindvalleyacademy/media/images/teaser-video-cover.jpg") else {
-      XCTAssert(false, "no image at url")
-      return
-    }
-    let cache = Cacher.sharedCache
-    let tokenFirst = cache.download(cacheType: .memory, url: imageURL) { (object: Data?, _) in }
-    let tokenSecond = cache.download(cacheType: .memory, url: imageURL) { (object: Data?, _) in }
-    XCTAssert(tokenFirst != tokenSecond, "token different")
-  }
-  
   func test_requestToken_cancel() {
     guard let imageURL = URL(string: "http://s75.mindvalley.us/mindvalleyacademy/media/images/teaser-video-cover.jpg") else {
       XCTAssert(false, "no image at url")
